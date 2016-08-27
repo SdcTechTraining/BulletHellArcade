2724bb7824c638cd1fe65adcaf42eff5dad7822e
Daniel Violet Sagmiller <Game.Dev@Live.com>
2016-08-27 10:36:07 -0500

    Added Player Prefab
    for issue 11

---
+++ Trunk/Source/BulletHell/Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundChecker : MonoBehaviour {
+
+	public GameObject Player { get; set; }
+	public bool IsGrounded { get; set; } 
+	
+    public void OnCollisionEnter(Collision col)
+    {
+		if (col.gameObject == Player)
+			return;
+
+		IsGrounded = true;
+    }
+
+    public void OnCollisionExit(Collision col)
+    {
+		if (col.gameObject == Player)
+			return;
+
+		IsGrounded = false;
+    }
+}
--- Trunk/Source/BulletHell/Assets/Scripts/Player.cs
+++ Trunk/Source/BulletHell/Assets/Scripts/Player.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 using System;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class Player : MonoBehaviour
 {
     public float Speed = 5f;
+    public float LookSpeed = 3f;
+	public float JumpForce = 10f;
+    public Transform Head;
 
-    // Use this for initialization
+    private new Rigidbody rigidbody = null;
+	private new CapsuleCollider collider = null;
+	private bool crouching = false;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-
+        rigidbody = GetComponent<Rigidbody>();
+		collider = GetComponent<CapsuleCollider> ();
     }
 
     void OnDestroy()
@@ -20,42 +28,68 @@ public class Player : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
-
-    // Update is called once per frame
+		
     void Update()
     {
         Look();
+        Move();
+        Escape();
+        Jump();
+		Crouch ();
+    }
 
+	private void Crouch()
+	{
+		if (!crouching && Input.GetKeyDown (KeyCode.LeftControl)) {
+			crouching = true;
+			collider.height *= 0.5f;
+			return;
+		}
+		if (crouching && Input.GetKeyUp (KeyCode.LeftControl)) {
+			crouching = false;
+			collider.height *= 2;
 
+		}
+	}
+
+    private void Jump()
+    {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (1)) {
+			
+			var grounded = Physics.Raycast (transform.position, Vector3.down, collider.bounds.extents.y + 0.1f);
+			if (grounded) {
+				rigidbody.AddRelativeForce (JumpForce * Vector3.up);
+			}
+		}
+    }
+
+    private void Escape()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.back * Speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * Speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * Speed * Time.deltaTime);
-        }
     }
 
     private void Look()
     {
-        var mouseX = Input.GetAxis("Mouse X");
-        var mouseY = Input.GetAxis("Mouse Y");
-        var mouseDelta = new Vector3(-mouseY, mouseX, 0f);
+        var mouseX = Input.GetAxis("Mouse X") * LookSpeed;
+        var mouseY = Input.GetAxis("Mouse Y") * LookSpeed;
+        var mouseDelta = new Vector3(-mouseY, 0f, 0f); 
+        Head.Rotate(mouseDelta, Space.Self);
+
+        mouseDelta = new Vector3(0f, mouseX, 0f);
         transform.Rotate(mouseDelta, Space.Self);
+    }
+
+    private void Move()
+    {
+        var adjustment = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) adjustment += Vector3.forward;
+        if (Input.GetKey(KeyCode.S)) adjustment += Vector3.back;
+        if (Input.GetKey(KeyCode.D)) adjustment += Vector3.right;
+        if (Input.GetKey(KeyCode.A)) adjustment += Vector3.left;
+        transform.Translate(adjustment * Speed * Time.deltaTime);
     }
 }
